c642d3d89966ec68cd66c7a3efb06d46417091a6
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-17 20:18:51 -0400

    implement login functionality with encrypted cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
@@ -26,9 +27,38 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
     // db operations
+    const { username, password } = req.body;
 
+    try{
+        // check if the user exists
+        const user = await prisma.user.findUnique({
+            where: {username}
+        });
+
+        if (!user) return res.status(401).json({message: "Invalid credentials"});
+
+        // check if the password is correct
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) return res.status(401).json({message: "Invalid credentials"});
+
+        // generate a cookie token and send it to the user
+        const age = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+        const token = jwt.sign({
+            id:user.id,
+        }, process.env.JWT_SECRET_KEY, {expiresIn: age});
+
+        res.cookie("token", token, {
+            httpOnly:true,
+            maxAge: age,
+        }).status(200).json({message: "Login successful"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Failed to login"});
+    }
 }
 
 export const logout = (req, res) => {
