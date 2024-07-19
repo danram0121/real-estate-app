a6feaaec8c7eaf152f8c08e5536f166e279d04aa
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-19 18:29:54 -0400

    initial structuring of listings page and stylesheet

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -1,9 +1,17 @@
+import { listData } from "../../lib/testdata";
 import "./listPage.scss";
 
 const listPage = () => {
+  const data = listData;
+
   return (
     <>
-      <div className="listPage">listPage</div>
+      <div className="listPage">
+        <div className="listContainer">
+          <div className="wrapper">List</div>
+        </div>
+        <div className="mapContainer">Map</div>
+      </div>
     </>
   );
 };
