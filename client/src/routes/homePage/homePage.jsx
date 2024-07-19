fdaca4c9323f077b9e7a8060aede89a860224c27
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-19 14:23:17 -0400

    implement home page and styling

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,41 @@
+import "./homePage.scss";
+import SearchBar from "../../components/searchBar/SearchBar";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo facere
+            veniam enim voluptate dolorum, quod nulla minus delectus odit esse
+            exercitationem soluta perferendis optio temporibus tempore
+            consequatur. Soluta, facere repudiandae!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Awards Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Properties Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
