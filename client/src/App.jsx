ba460742a8343c5a95f5392d1c44b731d5f6d0a5
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-19 18:17:25 -0400

    implement react-router and restructure layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,40 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import HomePage from "./routes/homePage/homePage";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import Login from "./routes/login/login";
+import ListDetail from "./routes/listDetail/listDetail";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/:id",
+          element: <ListDetail />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <>
-      <div className="layout">
-        <div className="navbar">
-          <Navbar />
-        </div>
-        <div className="content">
-          <HomePage />
-        </div>
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
