bdbcc82332f85c93058b4530f7c010ff8f918e34
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-20 02:43:59 -0400

    implement Leaflet map using setup example with mock data

---
+++ client/src/components/map/Map.jsx
@@ -0,0 +1,29 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./map.scss";
+import "leaflet/dist/leaflet.css";
+import Pin from "../pin/Pin";
+
+const Map = ({ items }) => {
+  return (
+    <>
+      <div className="">
+        <MapContainer
+          center={[52.5, -1.9]}
+          zoom={7}
+          scrollWheelZoom={true}
+          className="map"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {items.map((item) => (
+            <Pin key={item.id} item={item} />
+          ))}
+        </MapContainer>
+      </div>
+    </>
+  );
+};
+
+export default Map;
