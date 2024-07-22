65978a351065c2cb45d58e792e441b9a142d46bb
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-22 00:08:45 -0400

    filled out profile page details section using mock data

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -1,11 +1,41 @@
 import "./profilePage.scss";
+import { userData } from "../../lib/testdata";
+import List from "../../components/list/List";
 
-const profilePage = () => {
+const ProfilePage = () => {
   return (
     <>
-      <div>profilePage</div>
+      <div className="profilePage">
+        <div className="details">
+          <div className="wrapper">
+            <div className="title">
+              <h1>User Information</h1>
+              <button>Update Profile</button>
+            </div>
+            <div className="info">
+              <span>
+                Avatar: <img src={userData.img} alt="" />
+              </span>
+              <span>Username: {userData.name}</span>
+              <span>Email: {userData.email}</span>
+            </div>
+            <div className="title">
+              <h1>My Listings</h1>
+              <button>Add New Post</button>
+            </div>
+            <List />
+            <div className="title">
+              <h1>My Saves</h1>
+            </div>
+            <List />
+          </div>
+        </div>
+        <div className="chatContainer">
+          <div className="wrapper"></div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default profilePage;
+export default ProfilePage;
