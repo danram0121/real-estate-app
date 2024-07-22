4e79ef112843e2c6b5675ef3739698848eb7cb3a
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-22 00:05:58 -0400

    created list component to create list of card components with data

---
+++ client/src/components/list/List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "../card/Card";
+import { listData } from "../../lib/testdata";
+
+const List = () => {
+  return (
+    <>
+      <div className="list">
+        {listData.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default List;
