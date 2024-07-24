68bfaa5221d8d9fe581f2ee9e02038c9b46af4d2
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-23 21:22:31 -0400

    implemented logic to create new user

--- client/src/routes/register/Register.jsx
+++ client/src/routes/register/Register.jsx
@@ -1,19 +1,56 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
   return (
     <>
       <div className="registerPage">
         <div className="formContainer">
           <div className="wrapper">
-            <form>
+            <form onSubmit={handleSubmit}>
               <h1>Create an Account</h1>
-              <input type="text" placeholder="Username" />
-              <input type="text" placeholder="Email" />
-              <input type="text" placeholder="Password" />
+              <input
+                name="username"
+                type="text"
+                placeholder="Username"
+                required
+              />
+              <input name="email" type="text" placeholder="Email" required />
+              <input
+                name="password"
+                type="text"
+                placeholder="Password"
+                required
+              />
               <button>Register</button>
-              <Link to="/login">Do you have an account?</Link>
+              {error && <span className="error">{error}</span>}
+              <Link to="/login">Already have an account?</Link>
             </form>
           </div>
         </div>
