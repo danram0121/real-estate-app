0faf5d325db5a65788fa9dd3b65158caa04bf409
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-24 00:58:08 -0400

    implemented logic to login user and set local storage with user data

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,17 +1,59 @@
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const [IsLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        username,
+        password,
+      });
+
+      localStorage.setItem("user", JSON.stringify(res.data));
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <>
       <div className="loginPage">
         <div className="formContainer">
           <div className="wrapper">
-            <form>
+            <form onSubmit={handleSubmit}>
               <h1>Welcome Back</h1>
-              <input type="text" placeholder="Username" />
-              <input type="text" placeholder="Password" />
-              <button>Login</button>
+              <input
+                name="username"
+                type="text"
+                placeholder="Username"
+                required
+              />
+              <input
+                name="password"
+                type="text"
+                placeholder="Password"
+                required
+              />
+              <button disabled={IsLoading}>Login</button>
+              {error && <span className="error">{error}</span>}
               <Link to="/register">Don't have an account?</Link>
             </form>
           </div>
