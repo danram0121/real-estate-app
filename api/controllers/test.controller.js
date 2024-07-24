7aa06a2667a7dfb9e3dfd50b313341f2bcbdd27e
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-24 17:46:55 -0400

    remove code from shouldBeLoggedIn function that is now handled by middleware

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,13 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const shouldBeLoggedIn = async (req, res) => {
-    const token = req.cookies.token;
-
-    if (!token) return res.status(401).json({message: "Not authenticated"});
-
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-        if(err) return res.status(401).json({message: "Token is not Valid!"});
-    })
+    // console.log(req.userID);
 
     res.status(200).json({message: "You are logged in!"});
 };
