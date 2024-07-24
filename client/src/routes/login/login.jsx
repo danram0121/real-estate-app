46735f27a4eb39f8ea32269f05270aadcc17c9d9
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-23 20:23:08 -0400

    implement login page and style

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,9 +1,25 @@
 import "./login.scss";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <>
-      <div className=""></div>
+      <div className="loginPage">
+        <div className="formContainer">
+          <div className="wrapper">
+            <form>
+              <h1>Welcome Back</h1>
+              <input type="text" placeholder="Username" />
+              <input type="text" placeholder="Password" />
+              <button>Login</button>
+              <Link to="/register">Don't have an account?</Link>
+            </form>
+          </div>
+        </div>
+        <div className="imgContainer">
+          <img src="/bg.png" alt="" />
+        </div>
+      </div>
     </>
   );
 };
