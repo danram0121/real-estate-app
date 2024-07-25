89b95d62237d2658b9234fbf6c26cd1be405e687
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-25 03:52:42 -0400

    implemented logic and tested all controllers using Postman

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,46 +2,83 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import prisma from '../lib/prisma.js';
 
+
+// getUsers not used for this app but could be useful for future projects
+// or for admin users
 export const getUsers = async (req, res) => {
     console.log("getUsers");
 
     try {
-        
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "failed to get users"});
     }
-}
+};
 
 export const getUser = async (req, res) => {
-    console.log("getUser");
+    const id = req.params.id;
 
     try {
-        
+        const user = await prisma.user.findUnique({
+            where:  {id}
+        });
+        res.status(200).json(user);
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "failed to get user"});
     }
-}
+};
 
 export const updateUser = async (req, res) => {
-    console.log("updateUser");
+    const id = req.params.id;
+    const tokenUserId = req.userID
 
+    const {password, avatar, ...inputs} = req.body;
+
+    if (id !== tokenUserId) { 
+        return res.status(403).json({message: "Not Authorized"});
+    }
+
+    let updatedPassword = null; 
     try {
-        
+
+        if(password){
+            updatedPassword = await bcrypt.hash(password, 10); 
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: {id},
+            data: {
+                ...inputs,
+                ...(updatedPassword && {password: updatedPassword}), // if updatedPassword is not null, add password to data
+                ...(avatar && {avatar}),
+            }
+        })
+
+        res.status(200).json(updatedUser);
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "failed to update user"});
     }
-}
+};
 
 export const deleteUser = async (req, res) => {
-    console.log("deleteUser");
-    
+    const id = req.params.id;
+    const tokenUserId = req.userID
+
+    if (id !== tokenUserId) { 
+        return res.status(403).json({message: "Not Authorized"});
+    }
+
     try {
-        
+        await prisma.user.delete({
+            where: {id}
+        })
+        res.status(200).json({message: "user deleted"});
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "failed to delete user"});
     }
-}
+};
