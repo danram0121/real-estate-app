f40337ef9ad224ad261f5333f76f3d19bc07923b
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-25 18:48:55 -0400

    setup post controller skeleton

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,51 @@
+import prisma from "../lib/prisma.js";
+
+export const getPosts = async (req, res) => {
+    console.log("Get all posts");
+
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+export const getPost = async (req, res) => {
+    console.log("Get this post");
+
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+export const addPost = async (req, res) => {
+    console.log("New post");
+
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+export const updatePost = async (req, res) => {
+    console.log("Updates post");
+
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+export const deletePost = async (req, res) => {
+    console.log("Deleted post");
+
+    try {
+        
+    } catch (error) {
+        
+    }
+};
