caf5decbfbee428edbbbe1d950e2be679e1c8794
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-26 17:43:03 -0400

    implemented logic to average the items in the arrays latitude and longitude

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -5,25 +5,31 @@ import "leaflet/dist/leaflet.css";
 import Pin from "../pin/Pin";
 
 const Map = ({ items }) => {
+  const avgCoords =
+    items.length > 0
+      ? [
+          items.reduce((sum, item) => sum + parseFloat(item.latitude), 0) /
+            items.length,
+          items.reduce((sum, item) => sum + parseFloat(item.longitude), 0) /
+            items.length,
+        ]
+      : [39.98047, -100.50376];
+
   return (
-    <>
-      <div className="">
-        <MapContainer
-          center={[52.5, -1.9]}
-          zoom={7}
-          scrollWheelZoom={true}
-          className="map"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {items.map((item) => (
-            <Pin key={item.id} item={item} />
-          ))}
-        </MapContainer>
-      </div>
-    </>
+    <MapContainer
+      center={[String(avgCoords[0]), String(avgCoords[1])]}
+      zoom={4}
+      scrollWheelZoom={true}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map((item) => (
+        <Pin key={item.id} item={item} />
+      ))}
+    </MapContainer>
   );
 };
 
