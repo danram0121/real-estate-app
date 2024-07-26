8d2060c2585231cc991ffc995391d1a21d193d89
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-26 03:44:43 -0400

    update widget to support an array of images

--- client/src/components/uploadWidget/UploadWidget.jsx
+++ client/src/components/uploadWidget/UploadWidget.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 // Create a context to manage the script loading state
 const CloudinaryScriptContext = createContext();
 
-const UploadWidget = ({ uwConfig, setAvatar }) => {
+const UploadWidget = ({ uwConfig, setState }) => {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
@@ -33,7 +33,7 @@ const UploadWidget = ({ uwConfig, setAvatar }) => {
         (error, result) => {
           if (!error && result && result.event === "success") {
             console.log("Done! Here is the image info: ", result.info);
-            setAvatar(result.info.secure_url);
+            setState((prev) => [...prev, result.info.secure_url]);
           }
         }
       );
