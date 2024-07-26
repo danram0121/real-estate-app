edac41532be6fc45dfc871869f767d57045dc4db
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-26 19:40:35 -0400

    implement suspense to use fallback while await renders response or resolves error

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -2,10 +2,11 @@ import "./listPage.scss";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
 
 const ListPage = () => {
-  const posts = useLoaderData();
+  const data = useLoaderData();
 
   return (
     <>
@@ -13,14 +14,21 @@ const ListPage = () => {
         <div className="listContainer">
           <div className="wrapper">
             <Filter />
-            {posts.map((item) => (
-              <Card key={item.id} item={item} />
-            ))}
+            <Suspense fallback={<p>Loading...</p>}>
+              <Await
+                resolve={data.postResponse}
+                errorElement={<p>Error Loading posts!</p>}
+              >
+                {(postResponse) =>
+                  postResponse.data.map((post) => (
+                    <Card key={post.id} item={post} />
+                  ))
+                }
+              </Await>
+            </Suspense>
           </div>
         </div>
-        <div className="mapContainer">
-          <Map items={posts} />
-        </div>
+        <div className="mapContainer">{/* <Map items={posts} /> */}</div>
       </div>
     </>
   );
