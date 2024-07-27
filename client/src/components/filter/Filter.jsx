58ffc71a900d677d087cdd33d5d106be6634b6e5
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-26 20:50:44 -0400

    implement logic for filter to update query

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -1,22 +1,56 @@
 import "./filter.scss";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Filter = () => {
+  const [searchParams, setSearhParams] = useSearchParams();
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    minPrice: searchParams.get("minPrice") || 0,
+    maxPrice: searchParams.get("maxPrice") || 1000000,
+    bedroom: searchParams.get("bedroom") || 1,
+  });
+
+  const handleChange = (e) => {
+    setQuery({
+      ...query,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFilter = () => {
+    setSearhParams(query);
+  };
+
   return (
     <>
       <div className="filter">
         <h1>
-          Search results for <b>London</b>
+          Search results for <b>{searchParams.get("city")}</b>
         </h1>
         <div className="top">
           <div className="item">
             <label htmlFor="city">Location</label>
-            <input type="text" id="city" placeholder="City Location" />
+            <input
+              type="text"
+              id="city"
+              name="city"
+              placeholder="City Location"
+              onChange={handleChange}
+              defaultValue={query.city}
+            />
           </div>
         </div>
         <div className="bottom">
           <div className="item">
             <label htmlFor="type">Type</label>
-            <select name="type" id="type">
+            <select
+              name="type"
+              id="type"
+              onChange={handleChange}
+              defaultValue={query.type}
+            >
               <option value="" disabled>
                 any
               </option>
@@ -26,24 +60,31 @@ const Filter = () => {
           </div>
           <div className="item">
             <label htmlFor="property">Property</label>
-            <select name="type" id="type">
+            <select
+              name="property"
+              id="property"
+              onChange={handleChange}
+              defaultValue={query.property}
+            >
               <option value="" disabled>
                 any
               </option>
-              <option value="apartment">Apartment</option>
               <option value="house">House</option>
-              <option value="condo">Condo</option>
+              <option value="townhouse">Townhouse</option>
               <option value="condo">Condo</option>
               <option value="land">Land</option>
+              <option value="apartment">Apartment</option>
             </select>
           </div>
           <div className="item">
             <label htmlFor="minPrice">Min Price</label>
             <input
-              type="numebr"
+              type="number"
               id="minPrice"
               name="minPrice"
               placeholder="any"
+              onChange={handleChange}
+              defaultValue={query.minPrice}
             />
           </div>
           <div className="item">
@@ -53,13 +94,22 @@ const Filter = () => {
               id="maxPrice"
               name="maxPrice"
               placeholder="any"
+              onChange={handleChange}
+              defaultValue={query.maxPrice}
             />
           </div>
           <div className="item">
             <label htmlFor="bedroom">Bedroom</label>
-            <input type="number" id="bedroom" placeholder="any" />
+            <input
+              type="number"
+              id="bedroom"
+              name="bedroom"
+              placeholder="any"
+              onChange={handleChange}
+              defaultValue={query.bedroom}
+            />
           </div>
-          <button>
+          <button onClick={handleFilter}>
             <img src="/search.png" alt="" />
           </button>
         </div>
