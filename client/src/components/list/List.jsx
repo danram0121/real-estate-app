5560c8061cec1c8628772660290d4eaab365e5d1
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-27 02:18:45 -0400

    update List component to map posts prop

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,12 +1,11 @@
 import "./list.scss";
 import Card from "../card/Card";
-import { listData } from "../../lib/testdata";
 
-const List = () => {
+const List = ({ posts }) => {
   return (
     <>
       <div className="list">
-        {listData.map((item) => (
+        {posts.map((item) => (
           <Card key={item.id} item={item} />
         ))}
       </div>
