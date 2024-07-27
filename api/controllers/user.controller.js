a0dff754fc36be8dd875860c041b819b35b161fe
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-27 01:02:12 -0400

    Add functionality to save and remove posts

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -65,6 +65,44 @@ export const updateUser = async (req, res) => {
     }
 };
 
+export const savePost = async (req, res) => {
+    const postId = req.body.postId;
+    const tokenUserId = req.userID;
+
+    try {
+        const savedPost = await prisma.savedPost.findUnique({
+            where: {
+                userId_postId: {
+                    userId:tokenUserId,
+                    postId,
+                }
+            }
+        });
+
+        if(savedPost){
+            await prisma.savedPost.delete({
+                where: {
+                    id: savedPost.id,
+                }
+            })
+            res.status(200).json({message: "Post removed from saved list"});
+        }
+        else{
+            await prisma.savedPost.create({
+                data: {
+                    userId: tokenUserId,
+                    postId,
+                }
+            })
+            res.status(200).json({message: "Post saved to list"});
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "failed to save post"});
+    }
+};
+
 export const deleteUser = async (req, res) => {
     const id = req.params.id;
     const tokenUserId = req.userID
