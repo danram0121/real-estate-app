daa3a69b19333af9372c93d27d9087959c5be467
Daniel Ramirez <dan.ram0121@gmail.com>
2024-07-27 00:20:02 -0400

    folder naming is causing navbar and styling to be committed again

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+  const { currentUser } = useContext(AuthContext);
+
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>LamaEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Agents</a>
+      </div>
+      <div className="right">
+        {currentUser ? (
+          <div className="user">
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
+            <span>{currentUser.username}</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/login">Sign in</a>
+            <a href="/register" className="register">
+              Sign up
+            </a>
+          </>
+        )}
+
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" onClick={() => setOpen(!open)} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+          <a href="">Agents</a>
+          <a href="">Sign in</a>
+          <a href="">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
